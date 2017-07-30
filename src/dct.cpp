a0e2b48bfabf10513854f1bb7ce8e9f1b7353daa
Scott aka: Keldo <keldo@devworksosi.com>
2017-07-30 15:42:38 -0700

    Update dct.cpp
    Streamlined the application

--- src/dct.cpp
+++ src/dct.cpp
@@ -1,97 +1,106 @@
-#include <iostream>
-#include <string>
-#include <cstdlib>
 #include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+#include <sys/stat.h>
+#include <string>
 #include "version.h"
 
-int main()
-{
-    static char version[] = VERSION_MAJOR "." VERSION_MINOR "." VERSION_PATCH "." VERSION_BUILD;
-    static char timestamp[] = __DATE__ " " __TIME__;
-   
-    using std::string;
+int main() {
+
+	static char version[] = VERSION_MAJOR "." VERSION_MINOR "." VERSION_PATCH "." VERSION_BUILD;
+	static char timestamp[] = __DATE__ " " __TIME__;
+	
+    printf("Checking root directory structure\n");
+    struct stat projbuff;
+    int projects = 0;
+	struct stat pubbuff;
+    int publish = 0;
+	
+    if (stat("projects", &projbuff) != -1) {
+       if (S_ISDIR(projbuff.st_mode)) {
+          projects = 1;
+       }
+    } else {
+       system(("mkdir projects"));
+    }
+	
+    if (stat("publish", &pubbuff) != -1) {
+       if (S_ISDIR(pubbuff.st_mode)) {
+          publish = 1;
+       }
+    } else {
+       system(("mkdir publish"));
+    }
+	
+	printf("Directory structure confirmed!\n\n");
+	
+	using std::string;
     using std::cin;
     using std::cout;
-    string projectname;
+	string projectname;
+	
+	printf("\n");
+    printf("**** Created for Developers.***********************************\n");
+    printf("Coded by DevWorks OSI dot Com.                                *\n");
+    printf("Freedom in Open Source.                                       *\n");
+	printf("Please use versioning, IE: yourproject-1.1.0.                 *\n");
+	printf("The last number in your projects version is the recent build. *\n");
+	printf("Version 1.1.0 becomes 1.1.1 and so forth on each new build.   *\n");
+	printf("***************************************************************\n");
+	
+	// Let's begin work
+	std::cout << "Enter your project folders name: ";
+	std::cin >> projectname;
+	
+	// Create the projects folder in the publish directory
+	printf("Creating your projects folders.\n");
+	system(("mkdir publish/" + projectname).c_str());
+	system(("mkdir publish/" + projectname + "/hash").c_str());
+	system(("mkdir publish/" + projectname + "/hash/zip").c_str());
+	system(("mkdir publish/" + projectname + "/hash/gzip").c_str());
+	system(("mkdir publish/" + projectname + "/hash/sha1").c_str());
+	system(("mkdir publish/" + projectname + "/hash/sha256").c_str());
+	system(("mkdir publish/" + projectname + "/hash/bz2").c_str());
 
-    printf("\n");
-    printf("**** Created for Developers.****\n");
-    printf("Coded by TweDev dot Com.\n");
-    printf("Freedom in Open Source.\n");
-    printf("********************************\n");
-
-
-    std::cout << "Enter your project folders name: ";
-    std::cin >> projectname;
-
-    // Idea is to call tar, gzip and zip
-
-    // add the different parts into a string
-    // then convert to a char const* using c_str()
-
-    // create the project folder
-    printf("Creating your projects folders.\n");
-    system(("mkdir projects/" + projectname).c_str());
-    system(("mkdir projects/" + projectname + "/hash").c_str());
-    system(("mkdir projects/" + projectname + "/hash/zip").c_str());
-    system(("mkdir projects/" + projectname + "/hash/gzip").c_str());
-    system(("mkdir projects/" + projectname + "/hash/sha1").c_str());
-    system(("mkdir projects/" + projectname + "/hash/sha256").c_str());
-    system(("mkdir projects/" + projectname + "/hash/bz2").c_str());
-
-    printf("Compressing your application.\n");
-    system(("tar -cf " + projectname + ".tar " + projectname + "").c_str());
-    printf("Creating a gzip version.\n");
-    system(("gzip -9 " + projectname + ".tar ").c_str());
-    printf("Creating a zip version.\n");
-    system(("zip -rq " + projectname + ".zip " + projectname).c_str());
-    printf("Creating a bz2 version.\n");
-    system(("tar -cf " + projectname + ".tar " + projectname + "").c_str());
-    system(("bzip2 -zq --fast " + projectname + ".tar ").c_str());
-    
-    printf("\n\n");
-    printf("Hashing your Compressed Files.\n");
-    printf("\n\n");
-    printf("MD5.\n");
-    system(("md5sum " + projectname + ".zip > zip-md5.log").c_str());
+	printf("Compressing your application.\n");
+	system(("tar -cf " + projectname + ".tar " + projectname).c_str());
+	system(("tar -czf " + projectname + ".tar.gz " + projectname).c_str());
+	system(("zip -rq " + projectname + ".zip " + projectname).c_str());
+	system(("bzip2 -zq --fast " + projectname + ".tar ").c_str());
+	
+	printf("Hashing your application.\n");
+	system(("md5sum " + projectname + ".zip > zip-md5.log").c_str());
     system(("md5sum " + projectname + ".tar.gz > gzip-md5.log").c_str());
-    system(("md5sum " + projectname + ".tar.bz2 > bz2-md5.log").c_str());
-
-    printf("\n\n");
-    printf("SHA1.\n");
-    system(("sha1sum " + projectname + ".zip > zip-sha1.log").c_str());
+	system(("md5sum " + projectname + ".tar.bz2 > bz2-md5.log").c_str());
+	system(("sha1sum " + projectname + ".zip > zip-sha1.log").c_str());
     system(("sha1sum " + projectname + ".tar.gz > gzip-sha1.log").c_str());
-    system(("sha1sum " + projectname + ".tar.bz2 > bz2-sha1.log").c_str());
-
-    printf("\n\n");
-    printf("SHA256.\n");
-    system(("sha256sum " + projectname + ".zip > zip-sha256.log").c_str());
+	system(("sha1sum " + projectname + ".tar.bz2 > bz2-sha1.log").c_str());
+	system(("sha256sum " + projectname + ".zip > zip-sha256.log").c_str());
     system(("sha256sum " + projectname + ".tar.gz > gzip-sha256.log").c_str());
-    system(("sha256sum " + projectname + ".tar.bz2 > bz2-sha256.log").c_str());
-   
-    printf("\n\n");
-    printf("Publishing your application to the projects folder.\n");
-    system(("mv " + projectname + ".tar.gz projects/" + projectname + "/").c_str());
-    system(("mv " + projectname + ".zip projects/" + projectname + "/").c_str());
-    system(("mv " + projectname + ".tar.bz2 projects/" + projectname + "/").c_str());
-    
-    printf("\n\n");
-    printf("Publishing your MD5, SHA1, and SHA256 Hash Sums to the hash folder for your project.\n");
-
-    system(("mv zip-md5.log projects/" + projectname + "/hash/zip/").c_str());
-    system(("mv gzip-md5.log projects/" + projectname + "/hash/gzip/").c_str());
-    system(("mv bz2-md5.log projects/" + projectname + "/hash/bz2/").c_str());
-
-    system(("mv zip-sha1.log projects/" + projectname + "/hash/sha1/").c_str());
-    system(("mv gzip-sha1.log projects/" + projectname + "/hash/sha1/").c_str());
-    system(("mv bz2-sha1.log projects/" + projectname + "/hash/bz2/").c_str());
-
-    system(("mv zip-sha256.log projects/" + projectname + "/hash/sha256/").c_str());
-    system(("mv gzip-sha256.log projects/" + projectname + "/hash/sha256/").c_str());
-    system(("mv bz2-sha256.log projects/" + projectname + "/hash/bz2/").c_str());
-
-    printf("\n\n");
-    printf("ALL DONE!.\n");
-
-    return 0;
+	system(("sha256sum " + projectname + ".tar.bz2 > bz2-sha256.log").c_str());
+	
+	printf("Publishing your application.\n");
+	system(("mv " + projectname + ".tar.gz publish/" + projectname + "/").c_str());
+    system(("mv " + projectname + ".zip publish/" + projectname + "/").c_str());
+	system(("mv " + projectname + ".tar.bz2 publish/" + projectname + "/").c_str());
+	system(("mv zip-md5.log publish/" + projectname + "/hash/zip/").c_str());
+    system(("mv gzip-md5.log publish/" + projectname + "/hash/gzip/").c_str());
+	system(("mv bz2-md5.log publish/" + projectname + "/hash/bz2/").c_str());
+	system(("mv zip-sha1.log publish/" + projectname + "/hash/sha1/").c_str());
+    system(("mv gzip-sha1.log publish/" + projectname + "/hash/sha1/").c_str());
+	system(("mv bz2-sha1.log publish/" + projectname + "/hash/bz2/").c_str());
+	system(("mv zip-sha256.log publish/" + projectname + "/hash/sha256/").c_str());
+    system(("mv gzip-sha256.log publish/" + projectname + "/hash/sha256/").c_str());
+	system(("mv bz2-sha256.log publish/" + projectname + "/hash/bz2/").c_str());
+	
+	printf("Cleaning up.\n");
+	system(("rm -Rf " + projectname + ".tar").c_str());
+	system(("mv " + projectname + " projects/").c_str());
+	
+	printf("\n\n");
+	printf("All done.\n");
+	printf("Your application can be found in the publish directory.\n");
+	printf("\n\n");
+	
+	return 0;
 }
