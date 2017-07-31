292721aabaa5eb82e6dbe78b887d0eebad0d84ba
Keldo <keldo@devworksosi.com>
2017-07-30 17:16:49 -0700

    Updated config usun autocong plus headers

---
+++ config.h
@@ -0,0 +1,20 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "support@devworksosi.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Developers Compression Tool"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Developers Compression Tool 1.2.05"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "developers-compression-tool"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.2.05"
